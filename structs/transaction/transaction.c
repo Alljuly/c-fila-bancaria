008ebadb4e238dfc148b52088f57fbf15a78bcf2
alice <ajsc.elsie@gmail.com>
2024-08-15 18:52:40 -0300

    implementando transactions

--- structs/transaction/transaction.c
+++ structs/transaction/transaction.c
@@ -1,19 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "transaction.h"
 
-
-Transaction* createNode(int cod, int s){
-    Transaction *newTransaction = (Transaction*)malloc(sizeof(Transaction));
-    newTransaction->cod = cod;
-    newTransaction->seconds = s;
-
+Transaction *createNode(int cod, int s) {
+  Transaction *newTransaction = (Transaction *)malloc(sizeof(Transaction));
+  newTransaction->cod = cod;
+  newTransaction->seconds = s;
+  newTransaction->prox = NULL;
+  return newTransaction;
 };
 
-//Recebe uma transação e insere no final da lista
-void add(Transaction newTransaction, Transaction *queue);
+void addTransaction(Transaction *newTransaction, Transaction **queue) {
+  if (*queue == NULL) {
+    *queue = newTransaction;
+  } else {
+    Transaction *aux = *queue;
+    while (aux->prox != NULL) {
+      aux = aux->prox;
+    }
+    aux->prox = newTransaction;
+  }
+};
 
-//Apaga a primeira transação da lista
-void pop(Transaction **queue);
+void pop(Transaction **queue) {
+  if (queue == NULL) {
+    printf("Nenhuma transacao encontrada");
+  }
+  Transaction *aux = *queue;
+  *queue = (*queue)->prox;
+  free(aux);
+  return;
+};
 
-void getAll(Transaction *queue);
+void getAll(Transaction *queue) {
+
+  printf("+-------------------+---------------+\n");
+  printf("| Tipo de Transação |    Duração    |\n");
+  printf("+-------------------+---------------+\n");
+  while (queue != NULL) {
+    printf("| %-17d | %-13d |\n", queue->cod, queue->seconds);
+    printf("+-------------------+---------------+\n");
+    queue = queue->prox;
+  }
+}
+
+int main() {
+  Transaction *x = NULL;
+
+  getAll(x);
+
+  Transaction *x1 = createNode(1, 5);
+  getAll(x);
+  addTransaction(x1, &x);
+  getAll(x);
+  Transaction *x2 = createNode(2, 10);
+  addTransaction(x2, &x);
+  getAll(x);
+
+  return 0;
+}
--- structs/transaction/transaction.h
+++ structs/transaction/transaction.h
@@ -5,17 +5,9 @@
 
 typedef struct transaction {
     int cod;
-<<<<<<< HEAD
-    int s;
+    int seconds;
     struct transaction *prox;
 } Transaction;
-=======
-    int seconds;
-    Transaction *prox;
-};
-
-typedef struct transaction Transaction;
->>>>>>> 14dc0d44f13b7e0a2570d2b5489a5255406335fd
 
 
 Transaction* createTr(int cod, int seconds);
